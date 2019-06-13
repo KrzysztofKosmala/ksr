fb0a0bb909006f4e80f1148e16e1e7a00085e839
KrzysztofKosmala <37869082+KrzysztofKosmala@users.noreply.github.com>
2019-06-13 21:29:54 +0200

    Added niewiadomski

--- src/main/java/KNN/Metrics/Helpers/Niewiadomski.java
+++ src/main/java/KNN/Metrics/Helpers/Niewiadomski.java
@@ -1,5 +1,7 @@
 package KNN.Metrics.Helpers;
 
+import static java.lang.Math.*;
+
 public class Niewiadomski implements IDistanceBetweenStrings
 {
 
@@ -13,9 +15,41 @@ public class Niewiadomski implements IDistanceBetweenStrings
     @Override
     public double countDistance(String left, String right)
     {
-       //to implement
 
-        return 0;
+        int n;
+        int N;
+        int counter = 0;
+        String shorterWord;
+        String longerWord;
+
+        n = min(left.length(), right.length());
+        N = max(left.length(), right.length());
+
+        if(left.length() > right.length()) {
+            longerWord = left;
+            shorterWord = right;
+        }
+        else {
+            longerWord = right;
+            shorterWord = left;
+        }
+
+        // po n-gramach...
+        for(int i = 1; i <= n; i++) {
+            // po pierwszym słowie...
+            for(int j = 0; j < longerWord.length() - i + 1; j++) {
+                // po drugim słowie...
+                for(int k = 0; k < shorterWord.length() - i + 1; k++) {
+                    if(longerWord.substring(j, j + i).equals(shorterWord.substring(k, k + i))) {
+                        counter++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return (2 / ((pow(N, 2) + N)) ) * counter;
+
     }
 
 }
